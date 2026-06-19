091f98a0b96dee5210dfc1625377215579344d98
Samuel <samuelsimplicio5@gmail.com>
2026-06-18 22:03:53 -0300

    Improve list ergonomics by wrapping the list node into a list field

--- src/list.zig
+++ src/list.zig
@@ -3,13 +3,21 @@ const RC = @import("ref_counter.zig").RC;
 
 pub fn List(comptime T: type) type {
     return struct {
+        list: RC(LL),
+
+        const Self = @This();
+
         pub const Bucket = std.ArrayList(T);
+
+        pub fn deinit(self: *Self, gpa: std.mem.Allocator) void {
+            self.list.deinit(gpa);
+        }
 
         pub const LL = struct {
             /// The bucket where data is located.
             bucket: RC(Bucket),
             /// The linked list tail.
-            node_tail: ?RC(LL),
+            node_tail: ?Self,
             /// The bucket index where the LL starts. Counts backwards.
             start_index: usize,
             /// This linked list node length.
@@ -45,42 +53,44 @@ pub fn List(comptime T: type) type {
         fn initFromBucket(
             gpa: std.mem.Allocator,
             bucket: RC(Bucket),
-            ll_tail: ?RC(LL),
+            ll_tail: ?Self,
             start_index: usize,
             len: usize,
-        ) !RC(LL) {
+        ) !Self {
             var b = bucket.borrow() catch unreachable;
             errdefer b.deinit(gpa);
 
-            return try RC(LL).init(gpa, .{
-                .bucket = b,
-                .node_tail = ll_tail,
-                .start_index = start_index,
-                .len = len,
-            });
+            return .{
+                .list = try RC(LL).init(gpa, .{
+                    .bucket = b,
+                    .node_tail = ll_tail,
+                    .start_index = start_index,
+                    .len = len,
+                }),
+            };
         }
 
-        pub fn init(gpa: std.mem.Allocator, values: []const T) !RC(LL) {
+        pub fn init(gpa: std.mem.Allocator, values: []const T) !Self {
             var ll = try initRaw(gpa, values);
             errdefer ll.deinit(gpa);
-            return try RC(LL).init(gpa, ll);
+            return .{ .list = try RC(LL).init(gpa, ll) };
         }
 
-        pub fn initWithTail(gpa: std.mem.Allocator, values: []const T, ll_tail: *RC(LL)) !RC(LL) {
+        pub fn initWithTail(gpa: std.mem.Allocator, values: []const T, ll_tail: *Self) !Self {
             var ll = try initRaw(gpa, values);
             errdefer ll.deinit(gpa);
 
-            ll.node_tail = if (ll_tail.getUnwrap().len > 0)
-                try ll_tail.borrow()
+            ll.node_tail = if (ll_tail.list.getUnwrap().len > 0)
+                .{ .list = try ll_tail.list.borrow() }
             else
                 null;
 
-            return try RC(LL).init(gpa, ll);
+            return .{ .list = try RC(LL).init(gpa, ll) };
         }
 
-        fn borrow(ll_ref: ?RC(LL)) ?RC(LL) {
-            return if (ll_ref) |t|
-                t.borrow() catch unreachable
+        fn borrow(node: ?Self) ?Self {
+            return if (node) |t|
+                .{ .list = t.list.borrow() catch unreachable }
             else
                 null;
         }
@@ -101,41 +111,41 @@ pub fn List(comptime T: type) type {
             return createBucketWithCapacity(gpa, values, values.len);
         }
 
-        fn appendAssumeCapacity(ll: *LL, gpa: std.mem.Allocator, bucket_ref: RC(Bucket)) !RC(LL) {
+        fn newHead(ll: *LL, gpa: std.mem.Allocator, bucket_ref: RC(Bucket)) !Self {
             var node_tail = borrow(ll.node_tail);
             errdefer if (node_tail) |*t| t.deinit(gpa);
             return try initFromBucket(gpa, bucket_ref, node_tail, bucket_ref.getUnwrap().items.len - 1, ll.len + 1);
         }
 
-        pub fn append(ll_ref: *RC(LL), gpa: std.mem.Allocator, item: T) !RC(LL) {
-            const ll = ll_ref.getPtr() catch unreachable;
+        pub fn append(self: *Self, gpa: std.mem.Allocator, item: T) !Self {
+            const ll = self.list.getPtr() catch unreachable;
             var bucket_ref = ll.bucket;
 
             var bucket = bucket_ref.getPtrUnwrap();
             if (bucket.items.len > 0 and bucket.items.len - 1 != ll.start_index) {
                 // someone has already added data to the bucket
                 if (copy_threshold <= ll.len) {
-                    return try initWithTail(gpa, &[1]T{item}, ll_ref);
+                    return try initWithTail(gpa, &[1]T{item}, self);
                 }
 
                 const items = bucket_ref.getUnwrap().items;
                 const copy_slice = items[ll.start_index + 1 - ll.len .. ll.start_index + 1];
                 bucket_ref = try createBucketWithCapacity(gpa, copy_slice, copy_slice.len + 1);
                 bucket_ref.getPtrUnwrap().appendAssumeCapacity(item);
-                return try appendAssumeCapacity(ll, gpa, bucket_ref);
+                return try newHead(ll, gpa, bucket_ref);
             }
 
             // we have space to append to the bucket
             try bucket.append(gpa, item);
             errdefer _ = bucket.pop();
 
-            return try appendAssumeCapacity(ll, gpa, bucket_ref);
+            return try newHead(ll, gpa, bucket_ref);
         }
 
-        pub fn insert_at(ll_ref: *RC(LL), gpa: std.mem.Allocator, idx: usize, item: T) !RC(LL) {
-            if (idx == 0) return append(ll_ref, gpa, item);
+        pub fn insert_at(self: *Self, gpa: std.mem.Allocator, idx: usize, item: T) !Self {
+            if (idx == 0) return append(self, gpa, item);
 
-            const ll = ll_ref.getUnwrap();
+            const ll = self.list.getUnwrap();
             if (idx > ll.len) {
                 var node_tail = ll.node_tail orelse return error.IndexOutOfRange;
                 var tail_ll = try insert_at(&node_tail, gpa, idx - ll.len, item);
@@ -143,7 +153,7 @@ pub fn List(comptime T: type) type {
                 return try initFromBucket(gpa, ll.bucket, tail_ll, ll.start_index, ll.len);
             }
 
-            var head_node: ?RC(LL) = borrow(ll.node_tail);
+            var head_node: ?Self = borrow(ll.node_tail);
             errdefer if (head_node) |*t| t.deinit(gpa);
             if (idx < ll.len)
                 head_node = try initFromBucket(gpa, ll.bucket, head_node, ll.start_index - idx, ll.len - idx);
@@ -151,13 +161,13 @@ pub fn List(comptime T: type) type {
             return try initFromBucket(gpa, ll.bucket, head_node, ll.start_index, idx);
         }
 
-        pub fn head(ll_ref: RC(LL)) ?T {
-            const ll = ll_ref.getUnwrap();
+        pub fn head(self: Self) ?T {
+            const ll = self.list.getUnwrap();
             return if (ll.len == 0) null else ll.bucket.getUnwrap().items[ll.start_index];
         }
 
-        pub fn tail(ll_ref: *RC(LL), gpa: std.mem.Allocator) !?RC(LL) {
-            const ll = ll_ref.getUnwrap();
+        pub fn tail(self: *Self, gpa: std.mem.Allocator) !?Self {
+            const ll = self.list.getUnwrap();
             if (ll.len == 0) return null;
 
             var node_tail = borrow(ll.node_tail);
@@ -166,11 +176,11 @@ pub fn List(comptime T: type) type {
             return try initFromBucket(gpa, ll.bucket, node_tail, ll.start_index - 1, ll.len - 1);
         }
 
-        pub fn delete_at(ll_ref: *RC(LL), gpa: std.mem.Allocator, idx: usize) !RC(LL) {
-            const ll = ll_ref.getUnwrap();
+        pub fn delete_at(self: *Self, gpa: std.mem.Allocator, idx: usize) !Self {
+            const ll = self.list.getUnwrap();
 
             if (idx == 0 and ll.len > 0)
-                return try tail(ll_ref, gpa) orelse init(gpa, &[_]T{});
+                return try tail(self, gpa) orelse init(gpa, &[_]T{});
 
             if (idx >= ll.len) {
                 var ll_tail = ll.node_tail orelse return error.IndexOutOfRange;
@@ -178,9 +188,9 @@ pub fn List(comptime T: type) type {
                 var tail_ll = try delete_at(&ll_tail, gpa, idx - ll.len);
                 errdefer tail_ll.deinit(gpa);
 
-                if (tail_ll.getPtrUnwrap().len == 0) {
+                if (tail_ll.list.getPtrUnwrap().len == 0) {
                     defer tail_ll.deinit(gpa);
-                    var node_tail = borrow(tail_ll.getUnwrap().node_tail);
+                    var node_tail = borrow(tail_ll.list.getUnwrap().node_tail);
                     errdefer if (node_tail) |*t| t.deinit(gpa);
                     return try initFromBucket(gpa, ll.bucket, node_tail, ll.start_index, ll.len);
                 }
@@ -188,7 +198,7 @@ pub fn List(comptime T: type) type {
                 return try initFromBucket(gpa, ll.bucket, tail_ll, ll.start_index, ll.len);
             }
 
-            var head_node: ?RC(LL) = borrow(ll.node_tail);
+            var head_node: ?Self = borrow(ll.node_tail);
             errdefer if (head_node) |*t| t.deinit(gpa);
             if (ll.len > idx + 1)
                 head_node = try initFromBucket(gpa, ll.bucket, head_node, ll.start_index - idx - 1, ll.len - idx - 1);
@@ -196,38 +206,38 @@ pub fn List(comptime T: type) type {
         }
 
         pub const Iterator = struct {
-            root: RC(LL),
-            ll: ?RC(LL),
+            root: Self,
+            ll: ?Self,
             current: usize,
 
-            pub fn initNoBorrow(ll: RC(LL)) Iterator {
-                const current_ll = if (ll.getUnwrap().len > 0) ll else null;
+            pub fn initNoBorrow(ll: Self) Iterator {
+                const current_ll = if (ll.list.getUnwrap().len > 0) ll else null;
                 return .{
                     .root = ll,
                     .ll = current_ll,
-                    .current = ll.getUnwrap().start_index,
+                    .current = ll.list.getUnwrap().start_index,
                 };
             }
 
-            pub fn init(ll: *RC(LL)) Iterator {
-                const b = ll.borrow() catch unreachable;
-                const current_ll = if (b.getUnwrap().len > 0) b else null;
+            pub fn init(ll: *Self) Iterator {
+                const b = Self{ .list = ll.list.borrow() catch unreachable };
+                const current_ll = if (b.list.getUnwrap().len > 0) b else null;
                 return .{
                     .root = b,
                     .ll = current_ll,
-                    .current = ll.getUnwrap().start_index,
+                    .current = ll.list.getUnwrap().start_index,
                 };
             }
 
             pub fn next(iter: *Iterator) ?T {
-                const ll = (iter.ll orelse return null).getUnwrap();
+                const ll = (iter.ll orelse return null).list.getUnwrap();
                 const item = ll.bucket.getUnwrap().items[iter.current];
 
                 if (iter.current == ll.start_index + 1 - ll.len) {
                     const ll_tail = ll.node_tail;
                     iter.ll = ll_tail;
                     if (ll_tail) |t|
-                        iter.current = t.getUnwrap().start_index;
+                        iter.current = t.list.getUnwrap().start_index;
                 } else {
                     iter.current -= 1;
                 }
@@ -240,7 +250,7 @@ pub fn List(comptime T: type) type {
             }
         };
 
-        fn equalsSlice(ll: RC(LL), slice: []const T) bool {
+        fn equalsSlice(ll: Self, slice: []const T) bool {
             var iter = Iterator.initNoBorrow(ll);
 
             var i: usize = 0;
@@ -266,30 +276,30 @@ test "Append" {
     var single = try MyList.init(gpa, &[_]u32{7});
     defer single.deinit(gpa);
 
-    var new_list_0 = try MyList.append(&list, gpa, 4);
+    var new_list_0 = try list.append(gpa, 4);
     defer new_list_0.deinit(gpa);
 
-    var new_list_1 = try MyList.append(&new_list_0, gpa, 6);
+    var new_list_1 = try new_list_0.append(gpa, 6);
     defer new_list_1.deinit(gpa);
 
-    var new_list_2 = try MyList.append(&list, gpa, 5);
+    var new_list_2 = try list.append(gpa, 5);
     defer new_list_2.deinit(gpa);
 
-    var new_list_3 = try MyList.append(&list, gpa, 9);
+    var new_list_3 = try list.append(gpa, 9);
     defer new_list_3.deinit(gpa);
 
-    var new_list_4 = try MyList.append(&empty, gpa, 1);
+    var new_list_4 = try empty.append(gpa, 1);
     defer new_list_4.deinit(gpa);
 
-    var new_list_5 = try MyList.append(&single, gpa, 8);
+    var new_list_5 = try single.append(gpa, 8);
     defer new_list_5.deinit(gpa);
 
-    try std.testing.expect(MyList.equalsSlice(new_list_0, &[_]u32{ 4, 3, 2, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_1, &[_]u32{ 6, 4, 3, 2, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_2, &[_]u32{ 5, 3, 2, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_3, &[_]u32{ 9, 3, 2, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_4, &[_]u32{1}));
-    try std.testing.expect(MyList.equalsSlice(new_list_5, &[_]u32{ 8, 7 }));
+    try std.testing.expect(new_list_0.equalsSlice(&[_]u32{ 4, 3, 2, 1 }));
+    try std.testing.expect(new_list_1.equalsSlice(&[_]u32{ 6, 4, 3, 2, 1 }));
+    try std.testing.expect(new_list_2.equalsSlice(&[_]u32{ 5, 3, 2, 1 }));
+    try std.testing.expect(new_list_3.equalsSlice(&[_]u32{ 9, 3, 2, 1 }));
+    try std.testing.expect(new_list_4.equalsSlice(&[_]u32{1}));
+    try std.testing.expect(new_list_5.equalsSlice(&[_]u32{ 8, 7 }));
 }
 
 test "Insert" {
@@ -299,30 +309,30 @@ test "Insert" {
     var list = try MyList.init(gpa, &[_]u32{ 1, 2, 3 });
     defer list.deinit(gpa);
 
-    var new_list_0 = try MyList.insert_at(&list, gpa, 0, 9);
+    var new_list_0 = try list.insert_at(gpa, 0, 9);
     defer new_list_0.deinit(gpa);
 
-    var new_list_1 = try MyList.insert_at(&list, gpa, 1, 9);
+    var new_list_1 = try list.insert_at(gpa, 1, 9);
     defer new_list_1.deinit(gpa);
 
-    var new_list_2 = try MyList.insert_at(&list, gpa, 2, 9);
+    var new_list_2 = try list.insert_at(gpa, 2, 9);
     defer new_list_2.deinit(gpa);
 
-    var new_list_3 = try MyList.insert_at(&new_list_2, gpa, 3, 7);
+    var new_list_3 = try new_list_2.insert_at(gpa, 3, 7);
     defer new_list_3.deinit(gpa);
 
-    var new_list_4 = try MyList.insert_at(&new_list_2, gpa, 1, 5);
+    var new_list_4 = try new_list_2.insert_at(gpa, 1, 5);
     defer new_list_4.deinit(gpa);
 
-    var new_list_5 = try MyList.insert_at(&list, gpa, 3, 0);
+    var new_list_5 = try list.insert_at(gpa, 3, 0);
     defer new_list_5.deinit(gpa);
 
-    try std.testing.expect(MyList.equalsSlice(new_list_0, &[_]u32{ 9, 3, 2, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_1, &[_]u32{ 3, 9, 2, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_2, &[_]u32{ 3, 2, 9, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_3, &[_]u32{ 3, 2, 9, 7, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_4, &[_]u32{ 3, 5, 2, 9, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_5, &[_]u32{ 3, 2, 1, 0 }));
+    try std.testing.expect(new_list_0.equalsSlice(&[_]u32{ 9, 3, 2, 1 }));
+    try std.testing.expect(new_list_1.equalsSlice(&[_]u32{ 3, 9, 2, 1 }));
+    try std.testing.expect(new_list_2.equalsSlice(&[_]u32{ 3, 2, 9, 1 }));
+    try std.testing.expect(new_list_3.equalsSlice(&[_]u32{ 3, 2, 9, 7, 1 }));
+    try std.testing.expect(new_list_4.equalsSlice(&[_]u32{ 3, 5, 2, 9, 1 }));
+    try std.testing.expect(new_list_5.equalsSlice(&[_]u32{ 3, 2, 1, 0 }));
 }
 
 test "remove" {
@@ -347,42 +357,42 @@ test "remove" {
     var tail_list = try MyList.initWithTail(gpa, &[_]u32{ 1, 2, 3 }, &tail_node);
     defer tail_list.deinit(gpa);
 
-    var new_list_0 = try MyList.delete_at(&list, gpa, 0);
+    var new_list_0 = try list.delete_at(gpa, 0);
     defer new_list_0.deinit(gpa);
 
-    var new_list_1 = try MyList.delete_at(&list, gpa, 1);
+    var new_list_1 = try list.delete_at(gpa, 1);
     defer new_list_1.deinit(gpa);
 
-    var new_list_2 = try MyList.delete_at(&list, gpa, 2);
+    var new_list_2 = try list.delete_at(gpa, 2);
     defer new_list_2.deinit(gpa);
 
-    var new_list_3 = try MyList.delete_at(&list, gpa, 3);
+    var new_list_3 = try list.delete_at(gpa, 3);
     defer new_list_3.deinit(gpa);
 
-    var new_list_4 = try MyList.delete_at(&new_list_2, gpa, 2);
+    var new_list_4 = try new_list_2.delete_at(gpa, 2);
     defer new_list_4.deinit(gpa);
 
-    var new_list_5 = try MyList.delete_at(&list, gpa, 4);
+    var new_list_5 = try list.delete_at(gpa, 4);
     defer new_list_5.deinit(gpa);
 
-    var new_list_6 = try MyList.delete_at(&head_list, gpa, 0);
+    var new_list_6 = try head_list.delete_at(gpa, 0);
     defer new_list_6.deinit(gpa);
 
-    var new_list_7 = try MyList.delete_at(&tail_list, gpa, 3);
+    var new_list_7 = try tail_list.delete_at(gpa, 3);
     defer new_list_7.deinit(gpa);
 
-    var new_list_8 = try MyList.delete_at(&single, gpa, 0);
+    var new_list_8 = try single.delete_at(gpa, 0);
     defer new_list_8.deinit(gpa);
 
-    try std.testing.expect(MyList.equalsSlice(new_list_0, &[_]u32{ 4, 3, 2, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_1, &[_]u32{ 5, 3, 2, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_2, &[_]u32{ 5, 4, 2, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_3, &[_]u32{ 5, 4, 3, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_4, &[_]u32{ 5, 4, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_5, &[_]u32{ 5, 4, 3, 2 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_6, &[_]u32{ 3, 2, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_7, &[_]u32{ 3, 2, 1 }));
-    try std.testing.expect(MyList.equalsSlice(new_list_8, &[_]u32{}));
+    try std.testing.expect(new_list_0.equalsSlice(&[_]u32{ 4, 3, 2, 1 }));
+    try std.testing.expect(new_list_1.equalsSlice(&[_]u32{ 5, 3, 2, 1 }));
+    try std.testing.expect(new_list_2.equalsSlice(&[_]u32{ 5, 4, 2, 1 }));
+    try std.testing.expect(new_list_3.equalsSlice(&[_]u32{ 5, 4, 3, 1 }));
+    try std.testing.expect(new_list_4.equalsSlice(&[_]u32{ 5, 4, 1 }));
+    try std.testing.expect(new_list_5.equalsSlice(&[_]u32{ 5, 4, 3, 2 }));
+    try std.testing.expect(new_list_6.equalsSlice(&[_]u32{ 3, 2, 1 }));
+    try std.testing.expect(new_list_7.equalsSlice(&[_]u32{ 3, 2, 1 }));
+    try std.testing.expect(new_list_8.equalsSlice(&[_]u32{}));
 }
 
 test "Empty" {
@@ -392,17 +402,17 @@ test "Empty" {
     var list = try MyList.init(gpa, &[_]u32{});
     defer list.deinit(gpa);
 
-    var new_list_0 = try MyList.append(&list, gpa, 1);
+    var new_list_0 = try list.append(gpa, 1);
     defer new_list_0.deinit(gpa);
 
-    var new_list_1 = try MyList.insert_at(&list, gpa, 0, 5);
+    var new_list_1 = try list.insert_at(gpa, 0, 5);
     defer new_list_1.deinit(gpa);
 
-    try std.testing.expect(MyList.equalsSlice(list, &[_]u32{}));
-    try std.testing.expect(MyList.equalsSlice(new_list_0, &[_]u32{1}));
-    try std.testing.expect(MyList.equalsSlice(new_list_1, &[_]u32{5}));
-    try std.testing.expectError(error.IndexOutOfRange, MyList.insert_at(&list, gpa, 2, 9));
-    try std.testing.expectError(error.IndexOutOfRange, MyList.delete_at(&list, gpa, 0));
+    try std.testing.expect(list.equalsSlice(&[_]u32{}));
+    try std.testing.expect(new_list_0.equalsSlice(&[_]u32{1}));
+    try std.testing.expect(new_list_1.equalsSlice(&[_]u32{5}));
+    try std.testing.expectError(error.IndexOutOfRange, list.insert_at(gpa, 2, 9));
+    try std.testing.expectError(error.IndexOutOfRange, list.delete_at(gpa, 0));
 }
 
 test "tail" {
@@ -415,8 +425,8 @@ test "tail" {
     var list = try MyList.initWithTail(gpa, &[_]u32{ 1, 2, 3 }, &empty);
     defer list.deinit(gpa);
 
-    const empty_tail = try MyList.tail(&empty, gpa);
+    const empty_tail = try empty.tail(gpa);
 
-    try std.testing.expect(MyList.equalsSlice(list, &[_]u32{ 3, 2, 1 }));
+    try std.testing.expect(list.equalsSlice(&[_]u32{ 3, 2, 1 }));
     try std.testing.expect(empty_tail == null);
 }
