7fed8a1b4fcea92379291d929992e0b61c415f94
Samuel Simplicio <samuelsimplicio5@gmail.com>
2026-06-23 17:05:40 -0300

    Make object ref counted

--- src/object.zig
+++ src/object.zig
@@ -1,12 +1,18 @@
 const std = @import("std");
+const RC = @import("ref_counter.zig").RC;
 const Value = @import("value.zig").Value;
 const List = @import("list.zig").List(Value).LL;
 
-pub const Obj = union(enum) {
-    list: List,
+pub const Obj = struct {
+    kind: RC(Kind),
 
-    pub fn deinit(obj: *Obj, gpa: std.mem.Allocator) void {
-        switch (obj.*) {
+    const Kind = union(enum) {
+        list: List,
+    };
+
+    pub fn deinit(obj: *Obj, gpa: std.mem.Allocator) !void {
+        const v = try obj.kind.getPtr();
+        switch (v.*) {
             inline else => |*o| o.deinit(gpa),
         }
     }
