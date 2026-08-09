3d11635853eaa3644b019b6985ded1bf2db84b8d
Samuel Simplicio <samuelsimplicio5@gmail.com>
2026-08-09 18:17:01 -0300

    Improve sexpr printing

--- src/sexpr.c
+++ src/sexpr.c
@@ -54,13 +54,16 @@ bool sexpr_clone(sexpr_t* out, sexpr_t src, const sv_allocator_t* a)
     return true;
 }
 
-static bool sexpr_format_builder(sexpr_t sexpr, sv_str_builder* b, const sv_allocator_t* a)
+static bool sexpr_format_builder(sexpr_t sexpr, sv_str_builder* b, int ident, const sv_allocator_t* a)
 {
 #define CHECK(...) if (!(__VA_ARGS__)) return false
     switch (sexpr.tag) {
         case S_ATOM:
             return token_format_builder(sexpr.atom, b, a);
         case S_CONS:
+            CHECK(sv_strb_add_char(b, '\n', a) != -1);
+            for (int i = 0; i < ident; i++)
+                CHECK(sv_strb_add(b, "  ", 2, a) != -1);
             CHECK(sv_strb_add_char(b, '(', a) != -1);
 
             sv_vec_t(sexpr_t) cons = sexpr.cons;
@@ -69,7 +72,7 @@ static bool sexpr_format_builder(sexpr_t sexpr, sv_str_builder* b, const sv_allo
                     CHECK(sv_strb_add_char(b, ' ', a) != -1);
                 }
                 sexpr_t s = cons.arr[i];
-                CHECK(sexpr_format_builder(s, b, a));
+                CHECK(sexpr_format_builder(s, b, ident + 1, a));
             }
 
             return sv_strb_add_char(b, ')', a) != -1;
@@ -83,7 +86,7 @@ static bool sexpr_format_builder(sexpr_t sexpr, sv_str_builder* b, const sv_allo
 sv_str_t sexpr_format(sexpr_t sexpr, const sv_allocator_t* a)
 {
     sv_str_builder b = sv_strb_init();
-    if (!sexpr_format_builder(sexpr, &b, a)) {
+    if (!sexpr_format_builder(sexpr, &b, 0, a)) {
         sv_strb_deinit(&b, a);
         return sv_str_err();
     }
